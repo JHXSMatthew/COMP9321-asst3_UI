09d30a31b4f98478ca0116d06bb416da262868a9
JHXSMatthew <68638023@qq.com>
2018-05-28 22:11:30 +1000

    add missing about view

---
+++ src/views/AboutView.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+export default class AboutView extends Component{
+  
+  render(){
+    return(
+      <div  className='container'>
+        <br/>
+        <iframe src="https://docs.google.com/document/d/1VG6jT-MVwHRUdVyq5AyWCtQOA_iOUk4DBuaD4eFRQjU/edit?usp=sharing" width="100%" height='99999px' frameBorder="0"/>
+      </div>
+    )
+  }
+}
